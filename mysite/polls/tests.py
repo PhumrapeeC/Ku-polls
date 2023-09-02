de45c2d41b29e5a4d65a3ab27513654dab65ad1e
PhumrapeeC <phumrapeegenius@gmail.com>
2023-09-02 20:18:18 +0700

    Implenment tutorial part 5

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -5,6 +5,7 @@ from django.utils import timezone
 from django.urls import reverse
 from .models import Question
 
+
 def create_question(question_text, days):
     """
     Create a question with the given `question_text` and published the
