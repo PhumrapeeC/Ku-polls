2031bfdc95ca895c87d93099352795a2ebf069ca
Phumrapee Chaowanapricha <phumrapee.ch@ku.th>
2023-09-09 19:23:11 +0700

    fix directory layout

--- polls/urls.py
+++ polls/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-from django.urls import path, reverse_lazy
 from django.views.generic import RedirectView
 
 from . import views
@@ -10,6 +9,6 @@ urlpatterns = [
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
-    path('', RedirectView.as_view(url=reverse_lazy('polls:index')), name='base_redirect'),
-    path('polls/', views.IndexView.as_view(), name='index'),
+    path('', RedirectView.as_view(url='/polls/')),
+    path('polls/', views.IndexView.as_view(), name='index')
 ]
