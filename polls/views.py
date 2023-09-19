702be3832ccba9602f2bd6c70158c62b0b8f3881
PhumrapeeC <phumrapeegenius@gmail.com>
2023-09-19 17:46:33 +0700

    sorted polls by date

--- polls/views.py
+++ polls/views.py
@@ -22,11 +22,9 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """
-        Return the last five published questions (not including those set to be
-        published in the future).
+        Return all published questions, sorted by date (from newest to oldest).
         """
-        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
-
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')
 
 class DetailView(generic.DetailView):
     """
